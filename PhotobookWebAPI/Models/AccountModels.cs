2f840badfbffbbee8e660ef40ffa65ba4efa75f6
Jonas <au544262@post.au.dk>
2019-04-05 09:48:01 +0200

    Removed confirmpassword from registerhost in accountcontroller

--- PhotobookWebAPI/Models/AccountModels.cs
+++ PhotobookWebAPI/Models/AccountModels.cs
@@ -21,10 +21,6 @@ namespace PhotobookWebAPI.Models
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
   
             [Required]
             [Display(Name = "Name")]
