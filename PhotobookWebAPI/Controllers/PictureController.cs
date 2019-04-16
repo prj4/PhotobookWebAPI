127e0a80424aa88953a4d80b04f44c11d88196d2
Jonas <au544262@post.au.dk>
2019-04-16 08:59:19 +0200

    Picture controller created

---
+++ PhotobookWebAPI/Controllers/PictureController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PhotoBookDatabase.Model;
+
+namespace PhotobookWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PictureController : Controller
+    {
+       
+
+    }
+}
