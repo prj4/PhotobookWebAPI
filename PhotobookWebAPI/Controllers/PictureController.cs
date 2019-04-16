70ffb8527396d5e24896a380950cc54fdec8b3a5
Jonas <au544262@post.au.dk>
2019-04-16 10:25:43 +0200

    Actions in picture controller

--- PhotobookWebAPI/Controllers/PictureController.cs
+++ PhotobookWebAPI/Controllers/PictureController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PhotobookWebAPI.Models;
 using PhotoBookDatabase.Model;
 
 namespace PhotobookWebAPI.Controllers
@@ -13,7 +15,28 @@ namespace PhotobookWebAPI.Controllers
     [Authorize]
     public class PictureController : Controller
     {
-       
 
+        [AllowAnonymous]
+        [Route("GetImage")]
+        [HttpGet]
+        public IActionResult GetImage()
+        {
+            CurrentDirectoryHelpers.SetCurrentDirectory();
+
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "Pictures", "Udklip.PNG");
+
+            return PhysicalFile(file, "image/PNG");
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> InsertPicture(InsertPictureModel model)
+        {
+
+
+            
+            return Ok();
+        }
     }
 }
---
+++ PhotobookWebAPI/Models/PictureModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotobookWebAPI.Models
+{
+    public class InsertPictureModel
+    {
+
+        [Required]
+        public string PictureString { get; set; }
+      
+    }
+}
