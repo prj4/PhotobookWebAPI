99594ab5e7389fc2c0be1c772ed5f8ed261de6c1
Andreas <201608930@post.au.dk>
2019-04-23 09:04:52 +0200

    Guest returnering tweaket lidt

--- PhotobookWebAPI/Controllers/AccountController.cs
+++ PhotobookWebAPI/Controllers/AccountController.cs
@@ -358,7 +358,14 @@ namespace PhotobookWebAPI.Controllers
 
                     return new AccountModels.ReturnGuestModel
                     {
-                        Event = e,
+                        Event = new Event{
+                            Description = e.Description,
+                            EndDate = e.EndDate,
+                            StartDate = e.StartDate,
+                            Location = e.Location,
+                            Name = e.Name,
+                            Pin = e.Pin
+                        },
                         Name = guest.Name
                     };
 
