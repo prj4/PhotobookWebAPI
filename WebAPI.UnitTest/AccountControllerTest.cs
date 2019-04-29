8c146837588a34e12a4b4c5fdfe315796ded856b
Andreas <201608930@post.au.dk>
2019-04-29 14:38:47 +0200

    Test setup forsøg

--- WebAPI.UnitTest/AccountControllerTest.cs
+++ WebAPI.UnitTest/AccountControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NSubstitute;
 using NUnit.Framework;
+using PhotobookWebAPI.Controllers;
 using PhotoBook.Repository.EventRepository;
 using PhotoBook.Repository.GuestRepository;
 using PhotoBook.Repository.HostRepository;
@@ -13,15 +14,28 @@ namespace WebAPI.UnitTest
         private IEventRepository _eventRepo;
         private IHostRepository _hostRepo;
         private IGuestRepository _guestRepo;
+        private AccountController _uut;
 
         [SetUp]
         public void SetUp()
         {
+
             _eventRepo = Substitute.For<IEventRepository>();
             _hostRepo = Substitute.For<IHostRepository>();
             _guestRepo = Substitute.For<IGuestRepository>();
+            _uut = new AccountController();
         }
 
+        [Test]
+        public void GetUser_ValidUserNamer()
+        {
+            
+        }
 
+        [Test]
+        public void GetUser_InvalidUserNamer Get()
+        {
+
+        }
     }
 }
