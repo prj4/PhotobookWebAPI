04114df8b3abc6af8d30dd9ab19b776e6f23ac41
Andreas <201608930@post.au.dk>
2019-04-29 12:14:51 +0200

    ting

--- PhotobookWebAPI.UnitTest/AccountControllerTest.cs
+++ PhotobookWebAPI.UnitTest/AccountControllerTest.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using NUnit.Framework;
+using PhotoBook.Repository.EventRepository;
+using PhotoBook.Repository.GuestRepository;
+using PhotoBook.Repository.HostRepository;
 
 namespace PhotobookWebAPI.UnitTest
 {
     [TestFixture]
     public class AccountControllerTest
     {
+        private IEventRepository _eventRepo;
+        private IHostRepository _hostRepo;
+        private IGuestRepository _guestRepo;
+
         [SetUp]
         public void Setup()
         {
-
+            _eventRepo = Substitute.For
         }
     }
 }
