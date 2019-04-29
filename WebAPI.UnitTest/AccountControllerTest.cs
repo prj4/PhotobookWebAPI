fcb512bc0b0eb4628beab45698a1408a4575c414
Andreas <201608930@post.au.dk>
2019-04-29 13:22:38 +0200

    Tror endeligt der er fundet en fungerende test suite

---
+++ WebAPI.UnitTest/AccountControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using PhotoBook.Repository.EventRepository;
+using PhotoBook.Repository.GuestRepository;
+using PhotoBook.Repository.HostRepository;
+
+namespace WebAPI.UnitTest
+{
+    [TestFixture]
+    public class AccountControllerTest
+    {
+        private IEventRepository _eventRepo;
+        private IHostRepository _hostRepo;
+        private IGuestRepository _guestRepo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _eventRepo = Substitute.For<IEventRepository>();
+            _hostRepo = Substitute.For<IHostRepository>();
+            _guestRepo = Substitute.For<IGuestRepository>();
+        }
+
+
+    }
+}
