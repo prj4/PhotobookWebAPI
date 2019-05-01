720a7934fc75dc3e56c63d32e862b768d644a696
Andreas <201608930@post.au.dk>
2019-05-01 11:49:17 +0200

    1337

--- PhotobookWebAPI/Startup.cs
+++ PhotobookWebAPI/Startup.cs
@@ -131,7 +131,7 @@ namespace PhotobookWebAPI
                 app.UseHsts();
             }
 
-            app.UseCors(builder=>builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            app.UseCors(builder=>builder.WithOrigins("https://localhost:1337").AllowAnyMethod().AllowAnyHeader().AllowCredentials());
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
