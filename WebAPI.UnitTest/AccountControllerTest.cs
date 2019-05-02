b7f0f20a760502f15596a3720e821abebd48203e
Andreas <201608930@post.au.dk>
2019-05-02 11:08:48 +0200

    Prøver at sæte et test projekt op

--- WebAPI.UnitTest/AccountControllerTest.cs
+++ WebAPI.UnitTest/AccountControllerTest.cs
@@ -1,29 +1,27 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using NUnit.Framework;
 using PhotobookWebAPI.Controllers;
+using PhotobookWebAPI.Data;
 using PhotoBook.Repository.EventRepository;
 using PhotoBook.Repository.GuestRepository;
 using PhotoBook.Repository.HostRepository;
 
 namespace WebAPI.UnitTest
 {
-    [TestFixture]
     public class AccountControllerTest
     {
         private IEventRepository _eventRepo;
         private IHostRepository _hostRepo;
         private IGuestRepository _guestRepo;
-        private AccountController _uut;
 
         [SetUp]
         public void SetUp()
         {
-
             _eventRepo = Substitute.For<IEventRepository>();
             _hostRepo = Substitute.For<IHostRepository>();
             _guestRepo = Substitute.For<IGuestRepository>();
-            //_uut = new AccountController();
         }
 
         [Test]
