675ccecba467b4415ab9e3c3aa88b4e84e9a8eaa
Andreas <201608930@post.au.dk>
2019-05-02 11:46:01 +0200

    origin ændring til cors

---
+++ XWebAPITests/AccountControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XWebAPITests
+{
+    public class AccountControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
