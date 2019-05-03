7490e826a3ad4258ad01e2ddb4af678ddc423f90
Jonas <au544262@post.au.dk>
2019-05-03 11:10:56 +0200

    Doing stuff

--- WebAPIUnitTest/AccountControllerTest.cs
+++ WebAPIUnitTest/AccountControllerTest.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Net;
+using System.Threading.Tasks;
 using Castle.Core.Internal;
 using NSubstitute;
 using NUnit.Framework;
@@ -12,9 +14,11 @@ using Moq;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework.Internal;
+using System.Data.Entity;
 
 namespace Tests
 {
+
     [TestFixture]
     public class AccountControllerTest
     {
@@ -30,26 +34,54 @@ namespace Tests
 
         private AccountController _uut;
 
-        
+        private List<AppUser> _users = new List<AppUser>
+        {
+            new AppUser() { },
+            new AppUser() { }
+        };
+
+
 
         [SetUp]
         public void Setup()
         {
+
+
             _eventRepo = Substitute.For<IEventRepository>();
             _hostRepo = Substitute.For<IHostRepository>();
             _guestRepo = Substitute.For<IGuestRepository>();
 
-            _userManager = Mock.Of<UserManager<AppUser>>();
-            _signInManager = Mock.Of<SignInManager<AppUser>>();
+            _userManager = MockUserManager<AppUser>(_users).Object;
+            //_userManager = Mock.Of<UserManager<AppUser>>();
+            //_signInManager = Mock.Of<SignInManager<AppUser>>();
+
+           
 
             _uut = new AccountController(_userManager, _signInManager, _eventRepo, _hostRepo, _guestRepo);
         }
 
         [Test]
-        public async void accounts_get_returnsOK()
+        public async Task accounts_get_returnsOK()
         {
-            var actionResult = await _uut.GetAccounts();
-            Assert.That(actionResult.IsNullOrEmpty());
+            var accounts = await _uut.GetAccounts();
+            Assert.That(accounts.IsNullOrEmpty());
+
+
+        }
+
+
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> ls) where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+            mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+            mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
+            mgr.Setup(x => x.UpdateAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+
+            return mgr;
         }
     }
 }
